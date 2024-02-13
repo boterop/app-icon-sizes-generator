359706b07334d5568eaa73bfb36e1ec915ee9c03
Santiago Botero <boterop22@gmail.com>
2024-02-13 15:19:40 -0500

    Image transform tests

---
+++ tests/test_image_transform.py
@@ -0,0 +1,32 @@
+from bin.image_transform import ImageTransform
+import unittest
+import os
+
+FILE_FOLDER = "tests/"
+FILE_NAME = "image"
+FILE_EXT = ".jpg"
+FILE_PATH = FILE_FOLDER+FILE_NAME+FILE_EXT
+
+class TestImageTransform(unittest.TestCase):
+    
+    def test_open(self):
+        transform = ImageTransform(FILE_PATH)
+        self.assertEqual(transform.image.filename, FILE_PATH)
+    
+    def test_resize(self):
+        transform = ImageTransform(FILE_PATH)
+        transform.resize(500, 500)
+        self.assertNotEqual(transform.image.size, transform.new_image.size)
+        self.assertEqual((500, 500), transform.new_image.size)
+    
+    def test_save(self):
+        transform = ImageTransform(FILE_PATH)
+        transform.resize(500, 500)
+        test_image_path = FILE_FOLDER+FILE_NAME+"_test"+FILE_EXT
+        transform.save(test_image_path)
+        
+        self.assertTrue(os.path.exists(test_image_path))
+        self.assertTrue(os.path.isfile(test_image_path))
+
+if __name__ == '__main__':
+    unittest.main()
