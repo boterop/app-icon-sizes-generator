ab88453c2cce0c7fabe796c61eb106fbd82b91c9
Santiago Botero <boterop22@gmail.com>
2024-02-15 10:34:15 -0500

    Create image name folder

--- bin/image_transform.py
+++ bin/image_transform.py
@@ -1,15 +1,16 @@
 from PIL import Image
 
-class ImageTransform():
+
+class ImageTransform:
     def __init__(self, path: str):
         file = path.split("/")[-1]
         self.path = "/".join(path.split("/")[0:-1])
-        self.image_name = file.split(".")[0:-1]
+        self.image_name = ".".join(file.split(".")[0:-1])
         self.image_extension = file.split(".")[-1]
         self.image = Image.open(path)
 
     def resize(self, width: int, height: int):
         self.new_image = self.image.resize((width, height))
-        
+
     def save(self, path: str):
         self.new_image.save(path)
