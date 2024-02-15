fe9e38f264acee42ee023d758447ccc4c75deae0
Santiago Botero <98826652+boterop@users.noreply.github.com>
2024-02-15 11:14:43 -0500

    Create image name folder (#3)
    * Pillow installed
    
    * Image transform
    
    * Image transform tests
    
    * Coverage
    
    * Android sizes
    
    * Android tests
    
    * Create image name folder

--- bin/android.py
+++ bin/android.py
@@ -7,29 +7,34 @@ IMAGE_NAME = "ic_launcher"
 IMAGE_EXT = ".png"
 DPI = [("h", 72), ("m", 48), ("xh", 96), ("xxh", 144), ("xxxh", 192)]
 
-class Android():
+
+class Android:
     def __init__(self, transformer: ImageTransform | str):
         if isinstance(transformer, str):
             self.transformer = ImageTransform(transformer)
         elif isinstance(transformer, ImageTransform):
-            self.transformer = transformer        
-        
+            self.transformer = transformer
+
     def create(self):
         build_path = self.create_build_folder()
         for dpi in DPI:
             name, size = dpi
             print(build_path)
-            dpi_path = build_path+FOLDER_NAME+"-"+name+"dpi/"
+            dpi_path = build_path + FOLDER_NAME + "-" + name + "dpi/"
 
             if not os.path.exists(dpi_path):
                 os.mkdir(dpi_path)
             self.transformer.resize(size, size)
-            self.transformer.save(dpi_path+IMAGE_NAME+IMAGE_EXT)
-        
+            self.transformer.save(dpi_path + IMAGE_NAME + IMAGE_EXT)
+
     def create_build_folder(self):
         path = self.transformer.path
-        build_path = path+"/"+BUILD_FOLDER+"/"
-        if not os.path.exists(build_path):
-            os.mkdir(build_path)
+        image_name = self.transformer.image_name
+        build_path = path + "/" + BUILD_FOLDER + "/" + image_name + "/"
+
+        bread = ""
+        for folder in build_path.split("/"):
+            bread += folder + "/"
+            if not os.path.exists(bread):
+                os.mkdir(bread)
         return build_path
-    
